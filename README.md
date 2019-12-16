41ec68854c2cf7095f7098d0509c4aca817d8619
Filip <filip.spanberg@gmail.com>
2019-12-16 14:08:03 +0100

    Java webserver added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# trumplator
+x# trumplator
 
 A simple mash-up of APIs that allows you to translate Trump-tweets to Yodaspeak. Trust us, it's funny.
 
---
+++ server/src/API.java
@@ -0,0 +1,9 @@
+import static spark.Spark.*;
+
+public class API {
+
+    public static void main(String[] args) {
+        port(5000);
+        get("/", (req, res) -> "Hello World");
+    }
+}
