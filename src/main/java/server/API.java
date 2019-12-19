552aa0e7fc7f2b3e9ef921adc9e6e02abaf8d62a
Filip <filip.spanberg@gmail.com>
2019-12-19 15:04:13 +0100

    Kan ladda ner tweets

--- src/main/java/server/API.java
+++ src/main/java/server/API.java
@@ -1,17 +1,24 @@
 package server;
 
-import twitter4j.*;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
+import com.google.gson.Gson;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
-import java.io.*;
-import java.util.List;
+import java.util.Base64;
 
-import static server.FileHelper.*;
+import static server.APIHelper.*;
+
+import server.APIHelper.Key;
+
 import static spark.Spark.*;
 
 public class API {
-    private Twitter twitter = TwitterFactory.getSingleton();
+    private String bearerToken;
+    private Gson gson = new Gson();
+
+    public API() {
+
+    }
 
     public static void main(String[] args) {
         API api = new API();
@@ -21,54 +28,41 @@ public class API {
 
     private void startServer() {
 
+        port(5000);
+
         before((req, res) -> {
             res.header("Access-Control-Allow-Origin", "*");
         });
 
-        port(5000);
         get("/", (req, res) -> {
-            List<Status> statuses = twitter.getUserTimeline("realDonaldTrump");
-
-            return statuses.get(0).toString();
+            return getTweet();
         });
     }
 
     public void authenticate() {
-        String[] apiKeys = getApiKeys();
-        twitter.setOAuthConsumer(apiKeys[0], apiKeys[1]);
-        AccessToken token = readToken();
-        if (token == null) {
-            try {
+        TwitterToken accessToken = readToken();
+        if (accessToken == null) {
+            String consumer_key = URLEncoder.encodeUTF8(getKey(Key.API_key));
+            String consumer_secret = URLEncoder.encodeUTF8(getKey(Key.API_secret_key));
+            String bearer_token = Base64.getEncoder().encodeToString(String.format("%s:%s", consumer_key, consumer_secret).getBytes());
 
-                RequestToken requestToken = twitter.getOAuthRequestToken();
-                AccessToken accessToken = null;
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                while (null == accessToken) {
-                    System.out.println("Open the following URL and grant access to your account:");
-                    System.out.println(requestToken.getAuthorizationURL());
-                    System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
-                    String pin = br.readLine();
-                    try {
-                        if (pin.length() > 0) {
-                            accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-                        } else {
-                            accessToken = twitter.getOAuthAccessToken();
-                        }
-                    } catch (TwitterException te) {
-                        if (401 == te.getStatusCode()) {
-                            System.out.println("Unable to get the access token.");
-                        } else {
-                            te.printStackTrace();
-                        }
-                    }
-                }
-                writeToken(accessToken);
+            HttpResponse request = Unirest.post("https://api.twitter.com/oauth2/token")
+                    .header("Authorization", "Basic " + bearer_token)
+                    .header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+                    .header("accept", "application/json")
+                    .body("grant_type=client_credentials").asJson();
 
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        } else {
-            twitter.setOAuthAccessToken(token);
+            System.out.println(request.getStatusText());
+            accessToken = gson.fromJson(request.getBody().toString(), TwitterToken.class);
+            writeToken(accessToken);
         }
+        bearerToken = accessToken.getAccessToken();
+    }
+
+    private String getTweet() {
+        HttpResponse request = Unirest.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=realDonaldTrump&count=1&tweet_mode=extended")
+                .header("Authorization" , "Bearer " + bearerToken).asJson();
+        Tweet[] tweets = gson.fromJson(request.getBody().toString(), Tweet[].class);
+        return tweets[0].toString();
     }
 }
---
+++ src/main/java/server/Tweet.java
@@ -0,0 +1,9 @@
+package server;
+
+public class Tweet {
+    private String full_text;
+
+    public String toString() {
+        return full_text;
+    }
+}
